1a8fc0ec98245109d2b411a8663def59252b4881
Azamat Zorkanov <azamatzorkanov@MacBook-Air-Azamat.local>
2020-02-01 16:13:31 +0600

    modified sc context

--- src/modules/context/sc-context.js
+++ src/modules/context/sc-context.js
@@ -1,4 +1,5 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 export const StoreContext = createContext({});
 export const StoreProvider = StoreContext.Provider;
+export const useStore = useContext(StoreContext);
