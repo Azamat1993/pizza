d5ad816122b22b5a0594736bce561105fceeb811
Azamat Zorkanov <azamatzorkanov@MacBook-Air-Azamat.local>
2020-02-01 15:36:55 +0600

    added actions to sc-item

--- src/modules/store/sc-item.js
+++ src/modules/store/sc-item.js
@@ -1,9 +1,30 @@
-import { decorate, observable } from "mobx";
+import { decorate, observable, action, computed } from "mobx";
 
 class SCItem {
   quantity = 0;
+
+  constructor(price) {
+    this.price = price || 0;
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    if (this.quantity > 0) {
+      this.quantity--;
+    }
+  }
+
+  getTotalPrice() {
+    return this.quantity * this.price;
+  }
 }
 
 export default decorate(SCItem, {
-  quantity: observable
+  quantity: observable,
+  incrementQuantity: action,
+  decrementQuantity: action,
+  getTotalPrice: computed
 });
