79ef36c4fe8315722ad1a29a2308f9770bc1fac5
Azamat Zorkanov <azamatzorkanov@MacBook-Air-Azamat.local>
2020-02-01 16:02:40 +0600

    refactored sc list context

--- src/modules/context/sc-context.js
+++ src/modules/context/sc-context.js
@@ -1,5 +1,4 @@
 import { createContext } from "react";
-import SCList from "modules/store/sc-list";
 
 export const StoreContext = createContext({});
 export const StoreProvider = StoreContext.Provider;
