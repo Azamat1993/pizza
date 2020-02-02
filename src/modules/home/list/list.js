a8e61046c90a096e61dd521b4c1acbf584c59992
Azamat Zorkanov <azamatzorkanov@MacBook-Air-Azamat.local>
2020-02-02 09:46:20 +0600

    added list component for home

--- src/modules/home/list/list.js
+++ src/modules/home/list/list.js
@@ -1,5 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { Grid } from "@material-ui/core";
 
-export const List = () => {
-  return <div />;
+import { Item } from "modules/home/item";
+
+export const List = ({ items }) => {
+  return (
+    <Grid container xs={12}>
+      {items.map(item => {
+        return (
+          <Grid key={item.id} item>
+            <Item item={item} />
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+};
+
+List.propTypes = {
+  items: PropTypes.array
 };
