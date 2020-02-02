1054f429a51694883f0fdbe44d697a36bba0a0a1
Azamat Zorkanov <azamatzorkanov@MacBook-Air-Azamat.local>
2020-02-02 13:03:46 +0600

    aligned styles a bit

--- src/modules/home/style.js
+++ src/modules/home/style.js
@@ -3,8 +3,9 @@ import { makeStyles } from "@material-ui/core/styles";
 export const useStyles = makeStyles(theme => ({
   content: {
     maxWidth: 900,
-    marginTop: theme.spacing(3),
+    marginTop: theme.spacing(5),
     marginLeft: "auto",
-    marginRight: "auto"
+    marginRight: "auto",
+    marginBottom: theme.spacing(5)
   }
 }));
