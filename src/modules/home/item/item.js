7a9cb271716e5d86ebb42b08b4b51c532e830218
Azamat Zorkanov <azamatzorkanov@MacBook-Air-Azamat.local>
2020-02-02 09:40:14 +0600

    added markup for item in home folder

--- src/modules/home/item/item.js
+++ src/modules/home/item/item.js
@@ -1,5 +1,39 @@
 import React from "react";
+import {
+  Card,
+  IconButton,
+  CardContent,
+  Typography,
+  CardActions,
+  CardHeader
+} from "@material-ui/core";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 
-export const Item = () => {
-  return <div />;
+import { useStyles } from "./style";
+
+export const Item = ({ item }) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+        action={
+          <IconButton aria-label="settings">
+            <FavoriteIcon />
+          </IconButton>
+        }
+        title={item.title}
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {item.description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
 };
